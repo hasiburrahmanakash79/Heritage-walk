08fc773aff529185fb05aa1c3a5ff775bc608f99
hasiburrahmanakash79 <hasiburrahmanakash79@gmail.com>
2024-05-04 17:21:01 +0600

    make a ticket booking page

--- src/Pages/TicketBooking/TicketBooking.jsx
+++ src/Pages/TicketBooking/TicketBooking.jsx
@@ -1,11 +1,10 @@
-
-const About = () => {
+const TicketBooking = () => {
   return (
     <div>
-      <div className="bg-[url('https://i.ibb.co/6Z9KgFc/photo-1625216846235-c49ef7dd38c9-q-80-w-2938-auto.jpg')] bg-cover">
+      <div className="bg-[url('https://i.ibb.co/vXbhV7c/Post-2022-May-Statecation-Le-Meridien-3.jpg')] bg-cover">
         <div className="p-5 py-16 bg-black/20 text-white">
           <h2 className="text-3xl md:text-6xl font-semibold uppercase ps-20  mt-28">
-            About Us
+            Dhaka tickets & offers
           </h2>
         </div>
       </div>
@@ -13,4 +12,4 @@ const About = () => {
   );
 };
 
-export default About;
+export default TicketBooking;
--- src/Routers/Router.jsx
+++ src/Routers/Router.jsx
@@ -2,6 +2,7 @@ import { createBrowserRouter } from "react-router-dom";
 import Main from "../Layouts/Main";
 import Home from "../Pages/Home/Home";
 import About from "../Pages/About/About";
+import TicketBooking from "../Pages/TicketBooking/TicketBooking";
 
 const router = createBrowserRouter([
   {
@@ -15,6 +16,10 @@ const router = createBrowserRouter([
       {
         path: "/about",
         element: <About />,
+      },
+      {
+        path: "/booking",
+        element: <TicketBooking />,
       },
     ],
   },
