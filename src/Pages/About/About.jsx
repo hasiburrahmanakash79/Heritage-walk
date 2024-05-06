5a54298f56f1952730a2cdc962154d05884c0007
hasiburrahmanakash79 <hasiburrahmanakash79@gmail.com>
2024-05-06 13:28:00 +0600

    change text size

--- src/Pages/About/About.jsx
+++ src/Pages/About/About.jsx
@@ -4,7 +4,7 @@ const About = () => {
     <div>
       <div className="bg-[url('https://i.ibb.co/6Z9KgFc/photo-1625216846235-c49ef7dd38c9-q-80-w-2938-auto.jpg')] bg-cover">
         <div className="p-5 py-16 bg-black/20 text-white">
-          <h2 className="text-3xl md:text-6xl font-semibold uppercase ps-20  mt-28">
+          <h2 className="text-2xl md:text-6xl font-semibold uppercase md:ps-20 mt-14 md:mt-28">
             About Us
           </h2>
         </div>
--- src/Pages/TicketBooking/TicketBooking.jsx
+++ src/Pages/TicketBooking/TicketBooking.jsx
@@ -3,7 +3,7 @@ const TicketBooking = () => {
     <div>
       <div className="bg-[url('https://i.ibb.co/vXbhV7c/Post-2022-May-Statecation-Le-Meridien-3.jpg')] bg-cover">
         <div className="p-5 py-16 bg-black/20 text-white">
-          <h2 className="text-3xl md:text-6xl font-semibold uppercase ps-20  mt-28">
+          <h2 className="text-2xl md:text-6xl font-semibold uppercase md:ps-20 mt-14 md:mt-28">
             Dhaka tickets & offers
           </h2>
         </div>
