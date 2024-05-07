d248aac5750c2da1bbc8cfcccfc558078b062f5f
hasiburrahmanakash79 <hasiburrahmanakash79@gmail.com>
2024-05-07 16:16:12 +0600

    make cord details

--- src/components/UseCardDetails.jsx
+++ src/components/UseCardDetails.jsx
@@ -1,6 +1,76 @@
 const UseCardDetails = () => {
   return (
-    <div className="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden">
+    <div className="container mx-auto my-10">
+      <div className="grid grid-cols-4 gap-1">
+        <div className="col-span-2">
+          <img
+            src="https://cdn.britannica.com/97/189797-050-1FC0041B/Night-view-Dhaka-Bangladesh.jpg"
+            alt=" "
+            className="object-cover"
+          />
+        </div>
+        <div className="col-span-1 space-y-1">
+          <img
+            src="https://cdn.britannica.com/97/189797-050-1FC0041B/Night-view-Dhaka-Bangladesh.jpg"
+            alt=""
+          />
+          <img
+            src="https://cdn.britannica.com/97/189797-050-1FC0041B/Night-view-Dhaka-Bangladesh.jpg"
+            alt=""
+          />
+        </div>
+        <div className="col-span-1 space-y-1">
+          <img
+            src="https://cdn.britannica.com/97/189797-050-1FC0041B/Night-view-Dhaka-Bangladesh.jpg"
+            alt=""
+          />
+          <img
+            src="https://cdn.britannica.com/97/189797-050-1FC0041B/Night-view-Dhaka-Bangladesh.jpg"
+            alt=""
+          />
+        </div>
+      </div>
+      <div className="my-5">
+        <h1 className="text-3xl font-semibold">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab aliquid
+          fuga a, recusandae repellendus optio.
+        </h1>
+      </div>
+      <div className="grid grid-cols-3 gap-10">
+        <div className="bg-blue-gray-50 col-span-2 p-5">
+          <div className="grid grid-cols-2 gap-3">
+            <div>
+              <p className="text-xl font-semibold">Time</p>
+              <p>Lorem, ipsum dolor. Lorem, ipsum dolor.</p>
+            </div>
+            <div>
+              <p className="text-xl font-semibold">Time</p>
+              <p>Lorem, ipsum dolor. Lorem, ipsum dolor.</p>
+            </div>
+            <div>
+              <p className="text-xl font-semibold">Time</p>
+              <p>Lorem, ipsum dolor. Lorem, ipsum dolor.</p>
+            </div>
+            <div>
+              <p className="text-xl font-semibold">Time</p>
+              <p>Lorem, ipsum dolor. Lorem, ipsum dolor.</p>
+            </div> 
+          </div>
+        </div>
+        <div className="col-span-1 bg-red-100"></div>
+      </div>
+      <div>
+        <h1 className="text-3xl my-3 font-bold">About</h1>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Labore, corrupti expedita mollitia doloremque exercitationem assumenda iste quo totam dolorem amet accusantium hic, praesentium porro ducimus aliquid quibusdam aperiam quia a fuga quaerat corporis. Impedit dolores aperiam mollitia tempora esse minima quidem saepe vel, numquam quis omnis nam quam! Neque, ea commodi! Error temporibus perspiciatis exercitationem magnam sequi illo laudantium sunt. Error quas incidunt ipsa ratione quia nemo aliquam fuga. Aspernatur cumque optio soluta earum error eveniet praesentium iusto, eaque commodi dignissimos corporis! Alias nam laboriosam, ea vitae ex sit maxime voluptatibus nemo! Quis nemo nihil corrupti tempora quibusdam nesciunt recusandae!</p>
+      </div>
+    </div>
+  );
+};
+
+export default UseCardDetails;
+
+{
+  /* <div className="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden">
       <div className="md:flex">
         <div className="md:flex-shrink-0">
           <img
@@ -10,13 +80,16 @@ const UseCardDetails = () => {
           />
         </div>
         <div className="p-8">
-          <div className="uppercase tracking-wide text-sm text-indigo-500 font-semibold">Name</div>
-          <p className="mt-2 text-gray-600">Lorem ipsum dolor, sit amet consectetur molestiae.</p>
-          <button className="mt-4 bg-indigo-500 hover:bg-indigo-700 text-white font-bold py-2 px-4 rounded">Learn More</button>
+          <div className="uppercase tracking-wide text-sm text-indigo-500 font-semibold">
+            Name
+          </div>
+          <p className="mt-2 text-gray-600">
+            Lorem ipsum dolor, sit amet consectetur molestiae.
+          </p>
+          <button className="mt-4 bg-indigo-500 hover:bg-indigo-700 text-white font-bold py-2 px-4 rounded">
+            Learn More
+          </button>
         </div>
       </div>
-    </div>
-  );
-};
-
-export default UseCardDetails;
+    </div> */
+}
