d28866b490883b8c9b2c7168d582fedf66899618
hasiburrahmanakash79 <hasiburrahmanakash79@gmail.com>
2024-05-14 12:42:20 +0600

    make about board of member

--- src/Pages/About/Team.jsx
+++ src/Pages/About/Team.jsx
@@ -1,10 +1,66 @@
-
 const Team = () => {
-    return (
-        <div>
-            
+  return (
+    <div className="">
+        <h1 className="text-4xl font-bold text-center my-16">Our Team</h1>
+        <div className="grid grid-cols-3 gap-20">
+      <div className="text-center">
+        <div className="avatar">
+          <div className="w-40 rounded-full">
+            <img src="https://static.vecteezy.com/system/resources/thumbnails/008/442/086/small_2x/illustration-of-human-icon-user-symbol-icon-modern-design-on-blank-background-free-vector.jpg" />
+          </div>
         </div>
-    );
+        <h1 className="text-xl font-bold">Hasan Mahmud</h1>
+        <p>Managing director</p>
+      </div>
+      <div className="text-center">
+        <div className="avatar">
+          <div className="w-40 rounded-full">
+            <img src="https://static.vecteezy.com/system/resources/thumbnails/008/442/086/small_2x/illustration-of-human-icon-user-symbol-icon-modern-design-on-blank-background-free-vector.jpg" />
+          </div>
+        </div>
+        <h1 className="text-xl font-bold">Hasan Mahmud</h1>
+        <p>Managing director</p>
+      </div>
+      <div className="text-center">
+        <div className="avatar">
+          <div className="w-40 rounded-full">
+            <img src="https://static.vecteezy.com/system/resources/thumbnails/008/442/086/small_2x/illustration-of-human-icon-user-symbol-icon-modern-design-on-blank-background-free-vector.jpg" />
+          </div>
+        </div>
+        <h1 className="text-xl font-bold">Hasan Mahmud</h1>
+        <p>Managing director</p>
+      </div>
+      <div className="text-center">
+        <div className="avatar">
+          <div className="w-40 rounded-full">
+            <img src="https://static.vecteezy.com/system/resources/thumbnails/008/442/086/small_2x/illustration-of-human-icon-user-symbol-icon-modern-design-on-blank-background-free-vector.jpg" />
+          </div>
+        </div>
+        <h1 className="text-xl font-bold">Hasan Mahmud</h1>
+        <p>Managing director</p>
+      </div>
+      <div className="text-center">
+        <div className="avatar">
+          <div className="w-40 rounded-full">
+            <img src="https://static.vecteezy.com/system/resources/thumbnails/008/442/086/small_2x/illustration-of-human-icon-user-symbol-icon-modern-design-on-blank-background-free-vector.jpg" />
+          </div>
+        </div>
+        <h1 className="text-xl font-bold">Hasan Mahmud</h1>
+        <p>Managing director</p>
+      </div>
+      <div className="text-center">
+        <div className="avatar">
+          <div className="w-40 rounded-full">
+            <img src="https://static.vecteezy.com/system/resources/thumbnails/008/442/086/small_2x/illustration-of-human-icon-user-symbol-icon-modern-design-on-blank-background-free-vector.jpg" />
+          </div>
+        </div>
+        <h1 className="text-xl font-bold">Hasan Mahmud</h1>
+        <p>Managing director</p>
+      </div>
+      
+    </div>
+    </div>
+  );
 };
 
 export default Team;
