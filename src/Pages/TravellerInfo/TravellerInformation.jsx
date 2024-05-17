51a4f2d6af212447a75a43a0574b86242e52970f
hasiburrahmanakash79 <hasiburrahmanakash79@gmail.com>
2024-05-17 22:09:04 +0600

    change something

--- src/Pages/TravellerInfo/TravellerInformation.jsx
+++ src/Pages/TravellerInfo/TravellerInformation.jsx
@@ -1,7 +1,20 @@
+import { useEffect, useState } from "react";
 import InfoCard from "./InfoCard";
+import { Render } from "keep-render";
 
 
 const TravellerInformation = () => {
+
+ const [name, setName] = useState("akash")
+ const [loading, setLoading] =useState(true)
+ useEffect(()=> {
+  setTimeout(()=> {
+    setLoading(false)
+  }, 3000)
+ },[])
+ console.log(loading);
+ 
+
   return (
     <div>
       <div className="bg-[url('https://images.squarespace-cdn.com/content/v1/5876d8d6e3df28c4d83ae377/1485224208084-2QZTQMXNZGNF72C4MWMO/dhaka+new+market+rickshaws+2.jpg')] bg-cover">
@@ -10,6 +23,21 @@ const TravellerInformation = () => {
             Traveller Information
           </h2>
         </div>
+      </div>
+      <div>
+        {name == "akas" ? <div> my name is akash</div> : <>my name is shakeeb</>}
+        
+        <Render isLoading={loading}>
+        <Render.When isTrue={name == "akash"}>
+          my name is akash
+        </Render.When>
+        <Render.Else>
+          my name is shakeeb
+        </Render.Else>
+        <Render.Loading>
+          data loading
+        </Render.Loading>
+        </Render>
       </div>
       <div className="mx-auto container">
         <InfoCard/>
